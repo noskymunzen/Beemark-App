b51cd400a4ff535afb43a407edcaa729a426851a
noskymunzen <munzenmayer@outlook.com>
2023-02-21 16:03:30 -0300

    fix: changes export interface

--- services/profile/profile.types.ts
+++ services/profile/profile.types.ts
@@ -1,7 +1,5 @@
-interface UserData {
+export interface UserData {
   email?: string;
   name?: string;
   passwords?: { currentPassword: string; newPassword: string };
 }
-
-export type { UserData };
